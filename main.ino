362f87c1d41e448f5599e770f7c5ca83536b7762
Thomas <thomas.faure@totologos.com>
2016-03-24 22:21:19 +0100

    clean up main

--- main.ino
+++ main.ino
@@ -6,9 +6,7 @@
 
 
 int led2 = D7;
-volatile int pos = 200;
-volatile int pos2 = 200;
-volatile long timeIRQ = 0;
+
 
 // RFID pinout
 #define SS_PIN DAC
@@ -31,7 +29,7 @@ NixiesDriver nixies;
 rotaryCoder Rot1;
 rotaryCoder Rot2;
 
-bool test = false;
+
 
 
 
@@ -68,9 +66,7 @@ void setup() {
 
   pinMode(led2, OUTPUT);
 
-  //SPI.begin();
-  //SPI.setClockSpeed(100000);
-  //SPI.setDataMode(SPI_MODE0) ;
+
 
 
   nixies.SetBlink(1000,100);
@@ -78,91 +74,20 @@ void setup() {
 
 }
 
-// Next we have the loop function, the other essential part of a microcontroller program.
-// This routine gets repeated over and over, as quickly as possible and as many times as possible, after the setup function is called.
-// Note: Code that blocks for too long (like more than 5 seconds), can make weird things happen (like dropping the network connection).  The built-in delay function shown below safely interleaves required background activity, so arbitrarily long delays can safely be done if you need them.
-static uint8_t counter = 0 ;
-static int inc = 50;
-//static const uint32_t leds[] = {0x00000001, 0x00000002, 0x00000004, 0x40000000, 0x80000000 };
-static const uint32_t leds[] = {0x00000001, 0x00000001, 0x00000001, 0x00000001, 0x00000001 };
-
-static int leftregister = 0;
 
 void loop()
 {
 
-current_pos_iot = pos;
-  //Serial.print(pos);
-  //Serial.print("/");
-  //Serial.println(pos2);
-  //delay(250);
-
-  //return;
-
-  leftregister++;
-  if(leftregister == 10)
-  {
-    leftregister = 0;
-  }
-
-  //nixies.LoadShiftRegister((1<<1) + (1<<(0+10)) + (1<<(6+20)));
-  nixies.DispValue(333);
+  nixies.DispValue(current_tag_iot);
   nixies.SetBrightness(nixies.GetMaxBrightness()/2);
   nixies.CyclTask();
-
-  //counter++;
-  //if(counter == 5)
-  //{
-  //    counter = 0;
-  //}
-
-
-
-  // To blink the LED, first we'll turn it on...
-  //delay(50);
-  //digitalWrite(led2, HIGH);
-
-  //SPI.transfer(13);
-  //digitalWrite(D1, LOW);
-  //SPI.transfer(buffer_tx, buffer_rx, 5, endSpiTransfer);
-  //tone(D0,20000);
-  // We'll leave it on for 1 second...
-  //delay(50);
-  //noTone(A0);
-  //Serial.println("hello world!");
-  //Serial.println(mfrc522.PCD_GetAntennaGain());
-
-  //byte v = mfrc522.PCD_ReadRegister(mfrc522.VersionReg);
-  //Serial.print(F("Firmware Version: 0x"));
-  //Serial.print(v, HEX);
-
-  // Nixies management !
-
-
- /*if( inc > 0)
- {
-   if(nixies.GetBrightness() + inc > nixies.GetMaxBrightness())
-   {
-       inc = -50;
-   }
- }
- else
- {
-   if(nixies.GetBrightness() + inc <= 50)
-   {
-      inc = 50;
-   }
- }
- nixies.SetBrightness(nixies.GetBrightness() + inc);
- */
-
 
 
   // RFID management !
   delay(10);
   if ( mfrc522.PICC_IsNewCardPresent())
   {
-      //digitalWrite(led2, HIGH);
+      digitalWrite(led2, HIGH);
       if ( mfrc522.PICC_ReadCardSerial())
       {
 
@@ -178,7 +103,7 @@ current_pos_iot = pos;
   }
   else
   {
-      //digitalWrite(led2, LOW);
+      digitalWrite(led2, LOW);
       retry--;
       if(retry == 0)
       {
